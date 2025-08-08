70412fe46de9874dbd5d25271885888db5a37d9f
Marcus Alagar <mvala079@gmail.com>
2025-08-07 21:25:44 -0500

    Added some documentation, cleaned code
    Added some documentation and cleaned up some code. For now,
    think am going to leave this specific project here, and try using
    at least a few helper external crates in future projects.

--- README.md
+++ README.md
@@ -3,3 +3,14 @@ To make binary, run:
 ```shell
 cargo objcopy --release -- -O binary mqpro-blink.bin
 ```
+
+## Useful Commands
+To fully disassemble build:
+```shell
+cargo objdump --release -- -D
+```
+
+To see elf output:
+```shell
+cargo readobj --release -- -a
+```
--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,13 @@ use core::ptr;
 
 global_asm!(include_str!("start.s"));
 
-
+/// First function called, after the program starts
+/// at `_start` defined in `start.s`.
+///
+/// For the MQ Pro, zeroes the BSS section, turns on
+/// the blue onboard LED, then calls main.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn _cstart() -> ! {
+pub fn _cstart() {
     unsafe extern "C" {
         static mut __text_end: u8;
         static mut __bss_start: u8;
@@ -21,16 +25,32 @@ pub unsafe extern "C" fn _cstart() -> ! {
         let count = &raw const __bss_end as *const u8 as usize - &raw const __bss_start as *const u8 as usize;
         ptr::write_bytes(&raw mut __bss_start as *mut u8, 0, count);
 
-        let PD_CFG2: *mut usize = 0x2000098 as *mut usize;
-        let PD_DATA: *mut usize = 0x20000a0 as *mut usize;
+        let pd_cfg2 = 0x2000098 as *mut usize;
+        let pd_data = 0x20000a0 as *mut usize;
 
         // Config PD18 for output
-        ptr::write_volatile(PD_CFG2, 0x1ff);
+        ptr::write_volatile(pd_cfg2, 0x1ff);
         // Turn on PD18
-        ptr::write_volatile(PD_DATA, 1 << 18);
+        ptr::write_volatile(pd_data, 1 << 18);
     }
 
-    loop {}
+    main();
+}
+
+pub fn main() -> i32 {
+    let pd_data = 0x20000a0 as *mut usize;
+
+    // Should be a 50% duty cycle PWM
+    unsafe {
+        for _ in 1..100000 {
+            // Turn on PD18
+            ptr::write_volatile(pd_data, 1 << 18);
+            // Turn off PD18
+            ptr::write_volatile(pd_data, 0);
+        }
+    }
+
+    0
 }
 
 #[panic_handler]
